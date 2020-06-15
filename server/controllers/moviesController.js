24286072c0656371183944d23c0f989cd9ba58f4
jtown970 <jasonrtowner@gmail.com>
2020-06-15 10:16:39 -0700

    set up movies ctrl using massive keywords to insert delete get much easier

--- server/controllers/moviesController.js
+++ server/controllers/moviesController.js
@@ -1,6 +1,41 @@
 module.exports = {
-  getAllMovies: (req, res) => {},
-  getMovieById: (req, res) => {},
-  addMovie: (req, res) => {},
-  deleteMovie: (req, res) => {},
+  getAllMovies: async (req, res) => {
+    const db = req.app.get('db')
+
+    const movies = await db.movies.find()
+
+    res.status(200).send(movies)
+
+  },
+  getMovieById: async (req, res) => {
+    const db = req.app.get('db')
+    const {id} = req.params
+
+    const [movie] = await db.movies.find({id})
+
+    if(movie){
+      res.status(200).send(movie)
+    } else {
+      res.status(404).send('movie not found')
+    }
+  },
+
+  addMovie: async (req, res) => {
+    const db = req.app.get('db')
+
+    const {title, rating, year} = req.body
+
+    const newMovie = await db.movies.insert({title, rating, year})
+
+    res.status(200).send(newMovie)
+  },
+
+  deleteMovie: async (req, res) => {
+    const db = req.app.get('db')
+    const {id} = req.params
+
+    await db.movies.destroy({id})
+    res.status(200).send('deleted movie')
+
+  },
 }
